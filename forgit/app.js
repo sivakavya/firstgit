255447240d214719c282d6fa3ab5a015ed866f87
sivakavya <155805464+sivakavya@users.noreply.github.com>
2024-01-11 15:12:59 +0530

    Update app.js

--- forgit/app.js
+++ forgit/app.js
@@ -1,1 +1,16 @@
 console.log('i dont love git')
+const header = document.getElementById('header');
+header.style.backgroundColor='green';
+
+header.style.borderBottom ='3px solid orange';
+
+const mainheading = document.getElementById('main-heading');
+mainheading.textContent="Fruit World";
+mainheading.style.color = "orange";
+const basketheading = document.getElementById('basket-heading');
+basketheading.style.color="green";
+
+const thanks = document.getElementById('thanks');
+const paragraph = document.createElement('p');
+paragraph.textContent = "Please visit us again";
+thanks.appendChild(paragraph);
